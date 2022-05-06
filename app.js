9f69440fd907d60dba16fae25fa7c0137ebef961
hyoshincopy <hyoshincopy@nolbal.com>
2022-05-06 10:51:34 +0900

    master first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const app = express()
+
+
+app.get()
+/*
+
+    You should create NVM's working directory if it doesn't exist:
+
+  mkdir ~/.nvm
+
+Add the following to ~/.zshrc or your desired shell
+configuration file:
+
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+  [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+
+You can set $NVM_DIR to any location, but leaving it unchanged from
+/opt/homebrew/opt/nvm will destroy any nvm-installed Node installations
+upon upgrade/reinstall.
+
+Type `nvm help` for further information.
+==> Summary
+🍺  /opt/homebrew/Cellar/nvm/0.39.1_1: 9 files, 184.1KB
+==> `brew cleanup` has not been run in the last 30 days, running now...
+
+*/
